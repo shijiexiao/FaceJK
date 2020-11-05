f379cda21241ebafe5446290aaa25f24f19f7604
shijiexiao <shijiexiao@tencent.com>
2020-11-05 14:15:15 +0800

    Face Web RTC 人脸识别

---
+++ README.md
@@ -0,0 +1,16 @@
+# Face-webRTC
+
+FACE JK
+
+webRTC 实时实现人工智能  人脸识别  可以检测人脸的表情 年龄 性别
+以及高性能人脸捕捉
+
+
+
+保证你用到飞起
+
+npm i 
+
+cd 
+
+npm run serve
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home" style="text-align: center;">
+    <div>
+      <h1>
+        人工智能 人脸识别 表情检测
+      </h1>
+      <h2>
+        算法模型：ssdMobilenetv1、tinyFaceDetector、mtcnn
+      </h2>
+
+    </div>
+    <hr />
+    <img alt="JK logo" src="../assets/人脸识别.jpeg" />
+    <h1>
+      静态图像识别 、 video音频识别 、 实时识别人脸
+    </h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
